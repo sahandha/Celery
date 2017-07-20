40f662729c78c79433e8b8e1ca25e411f6dd7278
Sahand Hariri <sahand@meb-0220c-01.mechse.illinois.edu>
2017-07-19 20:20:34 -0500

    Initial celery commit

---
+++ celery_conf.py
@@ -0,0 +1,28 @@
+import os
+
+from celery import Celery
+
+# Get Kubernetes-provided address of the broker service
+broker_service_host = os.environ.get('RABBITMQ_SERVICE_SERVICE_HOST')
+
+app = Celery('tasks', broker='amqp://guest@%s//' % broker_service_host, backend='amqp')
+
+@app.task
+def add(x, y):
+    return x + y
+
+
+@app.task
+def add(x, y):
+    return x + y
+
+
+@app.task
+def multiply(x, y):
+    return x*y
+
+
+
+@app.task
+def divide(x, y):
+    return x/y
